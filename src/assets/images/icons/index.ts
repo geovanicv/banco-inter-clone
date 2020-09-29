027b8f81d0240d326dc003c7fe0cf2b1e6ce69f1
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-09-28 21:19:17 -0300

    :bento: chore: add icons

--- src/assets/images/icons/index.ts
+++ src/assets/images/icons/index.ts
@@ -1,7 +1,14 @@
-export { ReactComponent as BasketballIcon } from './basketball.svg';
-export { ReactComponent as BoletoIcon } from './boleto.svg';
-export { ReactComponent as CarIcon } from './car.svg';
-export { ReactComponent as HomeIcon } from './home.svg';
-export { ReactComponent as PlataformaPaiIcon } from './plataforma-pai.svg';
-export { ReactComponent as SmartphoneIcon } from './smartphone.svg';
-export { ReactComponent as TicketIcon } from './ticket.svg';
+// export { ReactComponent as BasketballIcon } from './basketball.svg';
+// export { ReactComponent as BoletoIcon } from './boleto.svg';
+// export { ReactComponent as CarIcon } from './car.svg';
+// export { ReactComponent as HomeIcon } from './home.svg';
+// export { ReactComponent as PlataformaPaiIcon } from './plataforma-pai.svg';
+// export { ReactComponent as SmartphoneIcon } from './smartphone.svg';
+// export { ReactComponent as TicketIcon } from './ticket.svg';
+
+export { ReactComponent as BarChartIcon } from './side-bar-bar-chart.svg';
+export { ReactComponent as DollarIcon } from './side-bar-dollar-sign.svg';
+export { ReactComponent as PieIcon } from './side-bar-piechart.svg';
+export { ReactComponent as ShieldIcon } from './side-bar-shield.svg';
+export { ReactComponent as BarSmartphoneIcon } from './side-bar-smartphone.svg';
+export { ReactComponent as UmbrellaIcon } from './side-bar-umbrella.svg';
